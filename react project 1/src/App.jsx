b10c7081dea7476673597ddcce02044f15e996ff
thinkGrow <rubaitreshad@gmail.com>
2024-12-02 09:03:08 +0600

    Written CSS for Nav, logo and login button alignment due

--- react project 1/src/App.jsx
+++ react project 1/src/App.jsx
@@ -1,26 +1,12 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+
 const App = () => {
-
-    return(
-        <div>
-           <nav>
-            <div className="logo">
-                <img src="/images/brand_logo.png" alt="logo" />
-            </div>
-
-            <ul>
-                <li href="#">Menu</li>
-                <li href="#">Location</li>
-                <li href="#">About</li>
-                <li href="#">Contact</li>
-            </ul>
-
-            <button>login</button>
-
-           </nav>
-        </div>
-    )
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
 };
 
 export default App;
-
-
