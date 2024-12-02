7c3b8006383bb2f3882574be8785db9be3a18ed1
thinkGrow <rubaitreshad@gmail.com>
2024-12-02 06:58:18 +0600

    html for nav

--- react project 1/src/App.jsx
+++ react project 1/src/App.jsx
@@ -2,7 +2,21 @@ const App = () => {
 
     return(
         <div>
-            My First Component
+           <nav>
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="logo" />
+            </div>
+
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+
+            <button>login</button>
+
+           </nav>
         </div>
     )
 };
