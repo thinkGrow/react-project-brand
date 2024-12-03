5e7b4cdfb3c0597506128f31c2ebb4622ad7ea9e
thinkGrow <rubaitreshad@gmail.com>
2024-12-03 09:40:02 +0600

    hero html

--- react project 1/src/App.jsx
+++ react project 1/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroSection from "./components/Hero";
 import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <HeroSection />
     </div>
   );
 };
